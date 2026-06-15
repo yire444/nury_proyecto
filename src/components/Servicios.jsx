938372079129b0516bc0c8ce5662c823fb45edb6
Yire <yire2005lol@gmail.com>
2026-06-14 21:54:53 -0500

    optimización de los estilos css

---
+++ src/components/Servicios.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const listServicios = [
+    {   
+        id: 1,
+        number: "01",
+        description: "",
+        deliverables: "",
+        img: "",
+        invested: false
+    }
+]
